30f00fdd58fed7d06748f155bb96811eb848aed4
Yani Asari <yani.iman1@gmail.com>
2020-09-09 22:20:50 +0000

    Done.

---
+++ lib/player.rb
@@ -0,0 +1,16 @@
+class Player 
+  
+#a root class that will act as inheritance point for actual player classes
+  
+  attr_reader :token 
+  
+  def initialize(token) 
+    @token = token 
+  end 
+  
+end 
+
+
+
+
+  
